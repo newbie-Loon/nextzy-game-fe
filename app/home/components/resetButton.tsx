a1a8ef4b81403d5d9ae079a314d48802ade94077
loon <n.yadav.2543@gmail.com>
2026-07-14 23:43:37 +0700

    prepare resetProgress func

--- app/home/components/resetButton.tsx
+++ app/home/components/resetButton.tsx
@@ -1,7 +1,20 @@
+import { GameService } from "@/app/service/game.service";
+import { CookieService } from "@/app/util/cookie.util";
+
 export default function ResetButton() {
+  const resetProgress = async ()=>{
+    const userCookie: string | null = CookieService.get("user") || null;
+    console.log("userCookie ", userCookie)
+    try{
+      await GameService.resetProgress(userCookie)
+      
+    }catch(e){
+      console.log(e)
+    }
+  }
   return (
     <div className="flex justify-center py-5">
-      <button className="rounded-full bg-blue-700 px-8 py-2 text-xl font-bold text-white">
+      <button className="rounded-full bg-[#1E00FF] px-8 py-2 text-xl font-bold text-white " onClick={resetProgress}>
         RESET
       </button>
     </div>
